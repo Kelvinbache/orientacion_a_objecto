272e2810095c02f1da982dfa2ce8715fd127cbcd
kelvin abche <kelvinabache12@gmail.com>
2023-09-01 18:05:40 -0400

    como crear un objecto

---
+++ class_1/index.js
@@ -0,0 +1,14 @@
+const user = {
+  name: "kelvin",
+  lastName: "abache", // clave:valor
+  age: 30,
+  hooble: ["read", "programming", "run"],
+  adress: {
+    city: "maracainboi",
+    strent: "san francisco",
+  },
+};
+
+console.log(user);
+
+// {}-> literamente estamos haciendo un objecto
